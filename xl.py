7ff1a718e8c1b89511a5f5073eea0a71e972f6bd
dimka77783 <91821862+dimka77783@users.noreply.github.com>
2024-05-28 01:12:55 +0300

    Add files via upload

--- xl.py
+++ xl.py
@@ -1,5 +1,6 @@
 import openpyxl
-
+from statistics import mean
+from openpyxl.workbook.defined_name import DefinedName
 path = "Работа с низкой мощностью(3).xlsx"
 wb_obj = openpyxl.load_workbook(path)  # Открываем файл
 sheet_obj1 = wb_obj["для свода 2"]
@@ -28,24 +29,19 @@ for n in range(2, m_row2 + 1):
     cell_obj3 = sheet_obj2.cell(row=n, column=2)  # мощность момент из листа мощность
     power_now = cell_obj3.value
     power_now_list.append(str(power_now)) # формируем список мощностей текущих
-print(date_start_list)
+#print(date_start_list)
 for date in date_start_list:
-    index_start = date_power_list.index(date)+2 # получаем номер строки
+    index_start = date_power_list.index(date)+2 # получаем номер строки даты начала
     index_start_list.append(str(index_start))
-    print(index_start)
+    #print(index_start)
 for date in date_stop_list:
-    index_stop = date_power_list.index(date)+2 # получаем номер строки
+    index_stop = date_power_list.index(date)+2 # получаем номер строки даты конца
     index_stop_list.append(str(index_stop))
 
-
-#проверка22
-
-#print(date_list)
-#cell_obj = sheet_obj2.cell(row=i, column=1)  # дата из листа мощность
-#date2 = cell_obj.value
-for n in range(3, m_row2 + 1):
-    cell_obj2 = sheet_obj2.cell(row=n, column=2)  # мощность момент из листа мощность
-    power_nom = cell_obj2.value
+"""
+for n in range(2, m_row2 + 1):
+    cell_obj4 = sheet_obj2.cell(row=n, column=2)  # мощность момент из листа мощность
+    power_nom = cell_obj4.value
     count = count + 1
     if isinstance(power_nom, float): # если мощность число с плавающей точкой
         pass# для всех значений из листа мощность
@@ -59,4 +55,30 @@ for n in range(3, m_row2 + 1):
 #sheats = wb_obj.sheetnames
 #for sheet in sheats:
     #print(sheet)
+"""
+for start, stop in zip(index_start_list, index_stop_list):
+    #new_range = f'C'+start+':C'+stop
+    mean = sheet_obj2.cell(row=start, column=3,value=)
+
+    #new_range = DefinedName('newrange', attr_text=f'мощность!С'+start+':C'+stop)
+    print(str(new_range22))
+    #mean_pow = mean(new_range)
+    #print(mean_pow)
+
+    #cell_obj4 = sheet_obj2.cell(row=int(x), column=2)  # мощность момент из листа мощность
+        #print(int(x))
+        #power_now1 = cell_obj4.value
+        #print(power_now1)
+        #if isinstance(power_now1, float): # если мощность число с плавающей точкой
+            #pass# для всех значений из листа мощность
+
+            #print(str(count)+" "+str(power_nom))
+            #if power_nom is float:
+            #print(str(count)+" "+str(power_nom))
+       # else:
+            #pass# print(str(count) + "не число")
+
+    #sheats = wb_obj.sheetnames
+    #for sheet in sheats:
+        #print(sheet)
 
